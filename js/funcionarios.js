803e5b667d81252853d728d088eea18f81af502b
Claudinei Martins <claudineimartins8@gmail.com>
2023-05-04 12:37:35 -0300

    Update funcionarios.js

--- js/funcionarios.js
+++ js/funcionarios.js
@@ -12,6 +12,93 @@ function toggleFormularioFuncionario (){
     
 
 }
+const inputsFormularioFuncionarios = document.querySelectorAll('[data-tipo]')
+inputsFormularioFuncionarios.forEach(input => {
+    const tipoDeInput = input.dataset.tipo;
+   input.addEventListener('blur', () => {
+    verificaInputValido(input)
+
+    })
+})
+const tiposDeErro = [
+    'badInput',
+    'customError',
+    'patternMismatch',
+    'rangeOverflow',
+    'rangeUnderflow',
+    'stepMismatch',
+    'tooShort',
+    'typeMismatch',
+    'valueMissing',
+    'tooLong',
+]
+const mensagensDeErro = {
+    nome: {
+        valueMissing:'O campo de nome Não pode estar vazio.',
+        tooShort: 'ta curto bro'
+    },
+    cargo: {
+        valueMissing:'O campo de nome Não pode estar vazio.',
+        tooShort: 'ta curto bro'
+    },
+    admissao: {
+        valueMissing:'O campo de nome Não pode estar vazio.',
+        tooShort: 'ta curto bro'
+    },
+    nome: {
+        valueMissing:'O campo de nome Não pode estar vazio.',
+        tooShort: 'ta curto bro'
+    }
+    
+
+}
+function mostraMensagemErro(input){
+    let tipoDeInput = input.dataset.tipo;
+    let mensagem = ""
+    console.log(tipoDeInput)
+    tiposDeErro.forEach(erro =>{
+        if(input.validity[erro]){
+        mensagem = mensagensDeErro[tipoDeInput][erro]
+        }
+    })
+
+    
+    
+    
+    
+    return mensagem
+    
+
+}
+
+
+function verificaInputValido(input){
+    if(input.validity.valid){
+        input.parentElement.classList.remove('container-form--invalido')
+        input.parentElement.querySelector('.mensagem-erro').innerHTML=""
+    }
+    else{
+        input.parentElement.classList.add('container-form--invalido')
+        input.parentElement.querySelector('.mensagem-erro').innerHTML=mostraMensagemErro(input)
+
+    }
+
+  
+   
+
+    tiposDeErro.forEach(erro=>{
+        if(input.validity[erro]){
+            console.log(input.validity[erro])
+            mostraMensagemErro(input,erro)
+            
+            console.log('ta erro')
+            // console.log(input.validity[erro])
+        }
+        
+        
+    })
+}
 
 
 function cadastraFormularioNaTabela(){}
+
